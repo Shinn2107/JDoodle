213118df68071923088d3e7e15fad9d348a19750
Shinn2107 <masterkc@hotmail.de>
2016-01-21 18:00:13 +0100

    initial upload

---
+++ JDoodleCore/src/com/devfriendly/service/JDoodleService.kt
@@ -0,0 +1,10 @@
+package com.devfriendly.service
+
+/**
+ * Created by Patrick Fey on 21.01.2016.
+ */
+interface JDoodleService{
+    fun createDoodle();
+    fun getDoodleInformation();
+}
+
---
+++ JDoodleCore/src/com/devfriendly/service/Test.java
@@ -0,0 +1,7 @@
+package com.devfriendly.service;
+
+/**
+ * Created by Patrick Fey on 21.01.2016.
+ */
+public class Test {
+}
